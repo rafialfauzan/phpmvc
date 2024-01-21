eba00a2cd807a6cba346b5b6eb0128e012ebbf12
Rafi Fauzan <rafifauzan976@gmail.com>
2024-01-21 10:15:39 +0800

    Mengubah file index home

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="jumbotron mt-4">
-        <h1 class="display-4">Selamat Datang di Website Saya</h1>
+        <h1 class="display-4">Selamat Datang di Website Saya Yang sangat bagus nan cantik</h1>
         <p class="lead">Halo, nama saya <?= $data['nama']?></p>
         <hr class="my-4">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus corporis molestias rerum doloribus ratione aliquid sapiente eligendi, officiis vitae veniam et numquam nobis quidem corrupti, repellat maxime? Debitis, repellat!</p>
