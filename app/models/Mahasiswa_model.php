6727f8e9c85eeb6f646f10b5ecbfedf8b21f703b
Rafi Fauzan <rafifauzan976@gmail.com>
2024-01-23 18:57:19 +0800

    membuat database wrap, mengubah view mahasiswa, dan membuat view detail mahasiswa

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,43 +1,21 @@
 <?php
 
 class Mahasiswa_model {
-    private $dbh;
-    private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
-    public function __construct() {
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try{
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch(PDOException $e){
-            die($e->getMessage());
-        }
-    } 
-    public function getAllMahasiswa(){
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function __construct(){
+        $this->db = new Database;
     }
-    // private $mhs = [
-    //     [
-    //         "nama" => "Rafi Fauzan",
-    //         "nrp" => "210030433",
-    //         "email" => "210030433@stikom-bali.ac.id",
-    //         "jurusan"=> "Sistem Informasi",
-    //     ],
-    //     [
-    //         "nama" => "Indra Suyoga",
-    //         "nrp" => "210030443",
-    //         "email" => "210030433@stikom-bali.ac.id",
-    //         "jurusan"=> "Sistem Informasi",
-    //     ],
-    //     [
-    //         "nama" => "Clarions Jonarence",
-    //         "nrp" => "210030305",
-    //         "email" => "210030305@stikom-bali.ac.id",
-    //         "jurusan"=> "Sistem Informasi",
-    //     ]
-    // ];
-
     
+    public function getAllMahasiswa(){
+        $this->db->query('SELECT * FROM '. $this->table);
+        return $this->db->resultSet();
+    }
+    
+    public function getMahasiswaById($id){
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id ');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
 }
